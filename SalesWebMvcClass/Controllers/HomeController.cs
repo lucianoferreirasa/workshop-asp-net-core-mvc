994598babd06e7f616c51ffe39ea86ebc72164bc
Luciano Ferreira <luciano-f-s-@hotmail.com>
2021-09-02 17:18:06 -0300

    MySQL adaptation and first migration

---
+++ SalesWebMvcClass/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvcClass.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvcClass.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Sales Web Mvc App from C# Course";
+            ViewData["Aluno"] = "Luciano Ferreira";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new SalesWebMvcClass.Models.ViewModels.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SalesWebMvcClass/Data/SalesWebMvcClassContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvcClass.Models;
+
+namespace SalesWebMvcClass.Data
+{
+    public class SalesWebMvcClassContext : DbContext
+    {
+        public SalesWebMvcClassContext (DbContextOptions<SalesWebMvcClassContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvcClass.Models.Department> Department { get; set; }
+    }
+}
