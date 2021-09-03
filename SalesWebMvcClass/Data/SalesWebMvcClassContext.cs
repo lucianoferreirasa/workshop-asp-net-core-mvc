981dacf929381a5578ae3daa431b86431ca8bb57
Luciano Ferreira <luciano-f-s-@hotmail.com>
2021-09-03 10:17:27 -0300

    Other entities and second migration

--- SalesWebMvcClass/Data/SalesWebMvcClassContext.cs
+++ SalesWebMvcClass/Data/SalesWebMvcClassContext.cs
@@ -14,6 +14,11 @@ namespace SalesWebMvcClass.Data
         {
         }
 
-        public DbSet<SalesWebMvcClass.Models.Department> Department { get; set; }
+
+
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+
     }
 }
--- SalesWebMvcClass/Migrations/20210903131511_OtherEntities.Designer.cs
+++ SalesWebMvcClass/Migrations/20210903131511_OtherEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesWebMvcClass.Data;
 
 namespace SalesWebMvcClass.Migrations
 {
     [DbContext(typeof(SalesWebMvcClassContext))]
-    partial class SalesWebMvcClassContextModelSnapshot : ModelSnapshot
+    [Migration("20210903131511_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
