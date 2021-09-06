4a2204efd6140dd3dd8d87030bccbc4fa83b4ba9
Luciano Ferreira <luciano-f-s-@hotmail.com>
2021-09-06 14:05:26 -0300

    Validation

--- SalesWebMvcClass/Controllers/SellersController.cs
+++ SalesWebMvcClass/Controllers/SellersController.cs
@@ -39,6 +39,10 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                return View(seller);
+            }
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -105,6 +109,10 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                return View(seller);
+            }
             if (id !=seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
--- SalesWebMvcClass/Models/Seller.cs
+++ SalesWebMvcClass/Models/Seller.cs
@@ -8,11 +8,18 @@ namespace SalesWebMvcClass.Models
     public class Seller
     {
         public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
